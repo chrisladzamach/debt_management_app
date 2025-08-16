cef51c73e29bc7bdbcd2793d1d41c572eb48f785
OrichalcosShunnoros <c3146092091@gmail.com>
2025-08-16 11:10:40 -0500

    Update: Header created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import { Header } from './components/Header/Header'
 import './App.css'
 
 export const App = () => {
   return (
     <div className='bg-black w-screen h-screen'>
-      <p className='text-2xl text-gray-400'>Test text</p>
+      <Header />
+      <section>
+        <article>
+
+        </article>
+        <article>
+          
+        </article>
+      </section>
     </div>
   )
 }
---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+
+export const Header = () => {
+  return (
+    <header>
+      <div>
+        <p className="text-2xl text-zinc-400">Debit Management Application</p>
+      </div>
+      <div>
+        <p className="text-2xl text-zinc-400">online</p>
+      </div>
+      <div>
+        <button className="text-2xl text-zinc-400 cursor-pointer bg-zinc-800 py-2 px-4 rounded-lg">
+          Add debit
+        </button>
+      </div>
+    </header>
+  )
+}
