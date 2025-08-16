536f4f256cb68750d009cc3d5d7fc016059695f8
OrichalcosShunnoros <c3146092091@gmail.com>
2025-08-16 11:47:10 -0500

    update: debt route code was added

--- backend/src/routes/debt.route.ts
+++ backend/src/routes/debt.route.ts
@@ -1,1 +1,10 @@
-//
+import { Router } from "express";
+import { debtController } from "../controllers/debt.controller";
+
+const router = Router();
+
+router.get("/", (req, res) => debtController.getAll(req, res));
+router.post("/", (req, res) => debtController.createDebt(req, res));
+router.post("/:id/payments", (req, res) => debtController.addPayment(req, res));
+
+export default router;
