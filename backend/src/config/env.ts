d1f888b0240ce15e833fa471d7d8dce720c0dd7a
OrichalcosShunnoros <c3146092091@gmail.com>
2025-08-16 11:21:02 -0500

    feat: add code for env.ts for cors config in app

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,1 +1,9 @@
-//
+import dotenv from "dotenv";
+dotenv.config();
+
+export const ENV = {
+  PORT: process.env.PORT ? Number(process.env.PORT) : 3000,
+  CORS_ORIGIN: (process.env.CORS_ORIGIN ?? "http://localhost:5173")
+    .split(",")
+    .map(s => s.trim())
+};
